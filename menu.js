5bec99ffa51df57331a9966f31b65b0e2d1f35af
nicklauftw <ncwlau@gmail.com>
2012-03-06 00:08:45 -0600

    Yay

--- menu.js
+++ menu.js
@@ -1,11 +1,1 @@
-// JavaScript Document
-$(function(){
-
-
-$("#menu select").change(function() {
-  window.location = $(this).find("option:selected").val();
-});
-
-	
-
-		 });//jquery end
+// JavaScript Document$(function(){$("#menu select").change(function() {  window.location = $(this).find("option:selected").val();});			 });//jquery end
--- twitterAPI.js
+++ twitterAPI.js
@@ -1,37 +1,1 @@
-// Weather API. Ricardo Elias. 2012.
-
-
-$(function(){
-
-//all
-var AttachtoAllDiv=document.getElementById("twitterfeed")
-var FeedUrl="http://twitter.com/statuses/user_timeline/ChicagoBreaking.rss"
-var LimitOfFeed=1
-var rssoutput="<b>  Breaking News: </b>"
-
-
-
-
-function rssfeedsetupAll(){
-var feedpointer=new google.feeds.Feed(FeedUrl) //Google Feed API method
-feedpointer.setNumEntries(LimitOfFeed) //Google Feed API method
-feedpointer.load(DisplayFeed) //Google Feed API method
-}
-
-
-function DisplayFeed(result){
-
-var AllTheFeeds=result.feed.entries
-for (var i=0; i<AllTheFeeds.length; i++)
-rssoutput+="<a href='" + AllTheFeeds[i].link + "'>" + AllTheFeeds[i].title + "</a>"  
-AttachtoAllDiv.innerHTML=rssoutput
-
-}
-
-
-//
-window.onload=function(){
-rssfeedsetupAll();
-
-}
-}); 
+// Weather API. Ricardo Elias. 2012.$(function(){//allvar AttachtoAllDiv=document.getElementById("twitterfeed")var FeedUrl="http://twitter.com/statuses/user_timeline/ChicagoBreaking.rss"var LimitOfFeed=1var rssoutput="<b>  Breaking News: </b>"function rssfeedsetupAll(){var feedpointer=new google.feeds.Feed(FeedUrl) //Google Feed API methodfeedpointer.setNumEntries(LimitOfFeed) //Google Feed API methodfeedpointer.load(DisplayFeed) //Google Feed API method}function DisplayFeed(result){var AllTheFeeds=result.feed.entriesfor (var i=0; i<AllTheFeeds.length; i++)rssoutput+="<a href='" + AllTheFeeds[i].link + "'>" + AllTheFeeds[i].title + "</a>"  AttachtoAllDiv.innerHTML=rssoutput}//window.onload=function(){rssfeedsetupAll();}}); 
